7c2d9d663fdf34d847e8ad5aa18095b762d751f6
sally <sally@gmail.com>
2024-09-21 16:30:38 +0800

    更新密碼

--- user/views.py
+++ user/views.py
@@ -5,10 +5,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
-    if request.method == "GET":
-        form = UserCreationForm()
+    message = ""
+    form = UserCreationForm()
 
     if request.method == "POST":
         print(request.POST)
 
-    return render(request, "user/register.html", {"form": form})
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        # 密碼長度8
+        if len(password1) != 8 or len(password2) != 8:
+            message = "密碼長度不正確"
+        elif password2 != password1:
+            message = "兩次密碼不一樣"
+
+    return render(request, "user/register.html", {"form": form, "message": message})
